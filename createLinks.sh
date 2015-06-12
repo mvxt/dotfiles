d822aae2c0ee4a4de8061d22e0b165c092201a84
Michael Thanh <meta.orphic@gmail.com>
2015-06-11 19:59:09 -0600

    Modified the script to create links

--- createLinks.sh
+++ createLinks.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-ln -s .bash_logout ~/.bash_logout
-ln -s .bash_profile ~/.bash_profile
-ln -s .bashrc ~/.bashrc
-ln -s .emacs.d ~/.emacs.d
-ln -s .gem ~/.gem
-ln -s .gemrc ~/.gemrc
-ln -s .git-completion.bash ~/.git-completion.bash
-ln -s .gitconfig ~/.gitconfig
-ln -s .ngrok ~/.ngrok
-ln -s .profile ~/.profile
-ln -s .tmux.conf ~/.tmux.conf
+cd ~/
+ln -s dotfiles/.bash_logout .bash_logout
+ln -s dotfiles/.bash_profile .bash_profile
+ln -s dotfiles/.bashrc .bashrc
+ln -s dotfiles/.dmrc .dmrc
+ln -s dotfiles/.emacs .emacs
+ln -s dotfiles/.emacs.d .emacs.d
+ln -s dotfiles/.emacs.el .emacs.el
+ln -s dotfiles/.gem .gem
+ln -s dotfiles/.gemrc .gemrc
+ln -s dotfiles/.git .git
+ln -s dotfiles/.git-completion.bash .git-completion.bash
+ln -s dotfiles/.gitconfig .gitconfig
+ln -s dotfiles/.ngrok .ngrok
+ln -s dotfiles/.profile .profile
+ln -s dotfiles/.tmux.conf .tmux.conf
 
