08ab6ef7d34b1575818682704d6138d402af735b
Michael Thanh <meta.orphic@gmail.com>
2016-01-22 02:17:40 -0500

    Removed lame file

--- installSoftware.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-PACKAGES=(emacs git)
-
-sudo apt-get update # Latest package lists
-sudo apt-get upgrade # Actually updates to latest software if possible
-sudo apt-get install $(PACKAGES) -y # Installs all packages listed at top of file
