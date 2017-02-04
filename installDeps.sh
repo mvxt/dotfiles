6a001e293276da248b8171bf51716d12a0e33336
Michael Thanh <meta.orphic@gmail.com>
2017-02-03 17:09:45 -0700

    Latest

--- installDeps.sh
+++ installDeps.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
+# Installs packages
 PACKAGES=(
     git
     tmux
@@ -15,3 +16,11 @@ for p in $PACKAGES; do
     sudo apt-get install -y $p
     echo 'Installed ' $p
 done
+
+# Installs fuzzy file finder
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+
+# Installs emacs plugins
+git clone https://github.com/emacs-helm/helm.git ~/dotfiles/.emacs.d/plugins/helm
+git clone https://github.com/jwiegley/emacs-async.git ~/dotfiles/.emacs.d/plugins/async
+git clone --recursive https://github.com/joaotavora/yasnippet ~/dotfiles/.emacs.d/plugins/yasnippet
